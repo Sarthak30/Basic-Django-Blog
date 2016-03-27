16e820f84ccc26c0c803c8f71ae6cae3033541e6
Sarthak <sarthaknsit@gmail.com>
2016-03-27 18:38:37 +0530

    Admin view made informative

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
 
 class Post(models.Model):
 	STATUS_CHOICES = (
@@ -15,9 +19,10 @@ class Post(models.Model):
 	publish = models.DateTimeField(default=timezone.now)
 	created = models.DateTimeField(auto_now_add=True)
 	updated = models.DateTimeField(auto_now=True)
-	status = models.CharField(max_length=10,
-	choices=STATUS_CHOICES,
-	default='draft')
+	status = models.CharField(max_length=10,choices=STATUS_CHOICES,default='draft')
+
+	objects = models.Manager()
+	published = PublishedManager()
 	class Meta:
 		ordering = ('-publish',)
 	def __str__(self):
